3aca4d7b6e079a05ea01f148015b5601b3d114da
JaccoM <development@jaccomulders.nl>
2020-09-02 16:20:23 +0200

    Added platform specific code

--- components/Shared/Header.js
+++ components/Shared/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Platform
+} from 'react-native';
 
 import Colors from '../../constants/colors';
 
@@ -7,23 +12,36 @@ import TitleText from './TitleText';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase,
+            ...Platform.select({
+                ios: styles.headerIOS,
+                android: styles.headerAndroid
+            })
+        }}>
             <TitleText style={styles.headerText}>{props.title}</TitleText>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center',
     },
+    headerIOS: {
+        backgroundColor: 'white',
+        borderBottomColor: '#CCC',
+        borderBottomWidth: 1,
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary,
+    },
     headerText: {
-        color: 'white',
+        color: Platform.OS === 'ios' ? Colors.primary : 'white',
     }
 });
 
